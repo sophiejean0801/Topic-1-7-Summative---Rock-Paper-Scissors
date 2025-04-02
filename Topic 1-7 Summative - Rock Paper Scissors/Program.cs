9a67c54829ba8d6262b26264a21ded06c11f6274
Sophie Jean <363449299@GoTVDSB.ca>
2025-04-02 13:46:27 -0400

    Add project files.

---
+++ Topic 1-7 Summative - Rock Paper Scissors/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_1_7_Summative___Rock_Paper_Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
